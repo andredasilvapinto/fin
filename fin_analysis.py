dcec81617d966cd71d22dcb4709f92410a580056
André Pinto <mail@andresp.me>
2018-06-02 16:27:33 +0100

    Average first and last prices
    To decrease influence of any particular day on the return computation

--- fin_analysis.py
+++ fin_analysis.py
@@ -1,13 +1,13 @@
+import math
+from collections import defaultdict
 from datetime import datetime
 
 import fix_yahoo_finance as yf
 import matplotlib.pyplot as plt
-import math
 import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 from dateutil.relativedelta import relativedelta
-from collections import defaultdict
 
 DAILY_RET = 'daily_ret'
 YESTERDAY_ADJ_CLOSE = 'yesterday_adj_close'
@@ -18,25 +18,21 @@ yf.pdr_override()
 symb = {
     #    'SPY': {'name': 'S&P 500 (Ref)', 'cost': 0.1, 'w': 0.73},
     #    'IVV': {'name': 'iShares Core S&P 500 USD (Dist)', 'cost': 0.07, 'w': 0.27}
-    'CSPX.AS': {'name': 'iShares Core S&P 500 (Acc)', 'cost': 0.07, 'w': 0.20},
-    'CEMU.AS': {'name': 'iShares MSCI EMU (Acc)', 'cost': 0.33, 'w': 0},
-    'IMAE.AS': {'name': 'iShares MSCI Europe (Acc)', 'cost': 0.33, 'w': 0},
-    'MEUD.PA': {'name': 'Lyxor Stoxx Europe 600 (Acc)', 'cost': 0.07, 'w': 0.20},
+    'CSPX.AS': {'name': 'iShares Core S&P 500 (Acc)', 'cost': 0.07, 'w': 0.10},
+    #    'CEMU.AS': {'name': 'iShares MSCI EMU (Acc)', 'cost': 0.33, 'w': 0},
+    #    'IMAE.AS': {'name': 'iShares MSCI Europe (Acc)', 'cost': 0.33, 'w': 0},
+    'MEUD.PA': {'name': 'Lyxor Stoxx Europe 600 (Acc)', 'cost': 0.07, 'w': 0.10},
     #    'ERO.PA': {'name': 'SPDR MSCI Europe (Acc)', 'cost': 0.18},
     #    'XMME.DE': {'name': 'Xtrackers MSCI Emerging Markets (Acc)', 'cost': 0.20},
     #    'EMIM.AS': {'name': 'iShares Core MSCI Emerging Markets IMI (Acc)', 'cost': 0.25},
     #    'AEME.PA': {'name': 'Amundi ETF MSCI Emerging Markets UCITS ETF DR (Acc)', 'cost': 0.20},
     #    'IEMA.AS': {'name': 'iShares MSCI Emerging Markets (Acc)', 'cost': 0.68},
     'IWDA.AS': {'name': 'iShares Core MSCI World (Acc)', 'cost': 0.20, 'w': 0.50},
-    #    'DBXH.DE': {'name': 'X II Global Infl-Lnkd Bd 1C Hedge (Acc)', 'cost': 0.25, 'w': 0.99},
-    #    'DBZB.DE': {'name': 'Xtrackers Global Sovereign Hedge (Acc)', 'cost': 0.25, 'w': 0.01},
-    #    'IUSN.DE': {'name': 'iShares MSCI World Small Cap (Acc)', 'cost': 0.35, 'w': 0.01},
-    'ZPRS.DE': {'name': 'SPDR MSCI World Small Cap (Acc)', 'cost': 0.45, 'w': 0.10},
+    'DBXH.DE': {'name': 'X II Global Infl-Lnkd Bd 1C Hedge (Acc)', 'cost': 0.25, 'w': 0.15},
+    #    'DBZB.DE': {'name': 'Xtrackers Global Sovereign Hedge (Acc)', 'cost': 0.25, 'w': 0.10},
+    #    'IUSN.DE': {'name': 'iShares MSCI World Small Cap (Acc)', 'cost': 0.35, 'w': 0.15},
+    'ZPRS.DE': {'name': 'SPDR MSCI World Small Cap (Acc)', 'cost': 0.45, 'w': 0.15},
 }
-
-
-def get_dt_value(item):
-    return item.index.date[0], item.values[0]
 
 
 def weighted_return(w):
@@ -75,18 +71,23 @@ for ticker, meta in symb.items():
 
     data[DAILY_RET] = data.apply(weighted_return(1), 1)
 
-    first_dt, first_value = get_dt_value(data.head(1)[ADJUSTED_CLOSE])
-    last_dt, last_value = get_dt_value(data.tail(1)[ADJUSTED_CLOSE])
+    first_dt = data.head(1).index.date[0]
+    last_dt = data.tail(1).index.date[0]
 
-    return_pct = (last_value - first_value) / first_value * 100
-    return_pct_y = annualize(first_value, last_value, delta_years) * 100
+    one_month = relativedelta(months=1)
+    first_m_avg = data[start:start + one_month][ADJUSTED_CLOSE].mean()
+    last_m_avg = data[end - one_month:end][ADJUSTED_CLOSE].mean()
+
+    return_pct = (last_m_avg - first_m_avg) / first_m_avg * 100
+    return_pct_y = annualize(first_m_avg, last_m_avg, delta_years) * 100
     ln_ret_std_dev_y = annualized_log_ret(data[DAILY_RET]) * 100
 
+    portfolio['w'] += weight
     portfolio['ret'] += weight * return_pct
     portfolio['cost'] += weight * cost
     w_ret[ticker] = data.apply(weighted_return(weight), 1)
 
-    data_set.append([ticker, name, weight, first_dt, last_dt, first_value, last_value,
+    data_set.append([ticker, name, weight, first_dt, last_dt, first_m_avg, last_m_avg,
                      return_pct, return_pct_y, ln_ret_std_dev_y, cost])
 
 w_ret = w_ret.sum(axis=1)
@@ -94,10 +95,11 @@ w_ret_std_dev_y = annualized_log_ret(w_ret) * 100
 last = 100 + portfolio['ret']
 annualized_portfolio_ret = annualize(100, last, delta_years) * 100
 
-data_set.append(['ALL', 'Portfolio', 1, start, end, 100, last,
+data_set.append(['ALL', 'Portfolio', portfolio['w'], start, end, 100, last,
                  portfolio['ret'], annualized_portfolio_ret, w_ret_std_dev_y, portfolio['cost']])
 
-df = pd.DataFrame(data_set, columns=['ticker', 'name', 'weight', 'first day', 'last day', 'first value', 'last value',
+df = pd.DataFrame(data_set, columns=['ticker', 'name', 'weight', '1st day', '1st day',
+                                     '1st month avg price', 'last month avg price',
                                      'return %', 'return % y', 'ln std dev % y', 'cost'])
 
 print(df.to_string())
